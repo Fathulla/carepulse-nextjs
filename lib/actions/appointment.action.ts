c598d9b739776d077e54e9d1ae09076d69a635e0
Fathulla Rashidov <fathullarasidov239@gmail.com>
2024-08-07 16:20:08 +0500

    success page is ready

--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -1,17 +1,15 @@
-"use server"
+"use server";
 
 import { ID } from "node-appwrite";
 import { databases } from "../appwrite.config";
 import { serverTokens } from "@/utils/serverTokens";
 import { parseStringify } from "../utils";
 
-const {
-  databaseId,
-  appointmentCollectionId,
-} = serverTokens;
+const { databaseId, appointmentCollectionId } = serverTokens;
 
-
-export const createAppointment = async (appointment: CreateAppointmentParams) => {
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
   try {
     const newAppointment = await databases.createDocument(
       databaseId,
@@ -24,4 +22,18 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
   } catch (error) {
     console.log(error);
   }
-}
+};
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      databaseId,
+      appointmentCollectionId,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
